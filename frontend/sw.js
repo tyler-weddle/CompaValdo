95c6f12779482476b8010cf7b16237ce45d47591
Tyler Weddle <tyler.weddle@wsu.edu>
2026-08-03 22:12:17 -0700

    sw.js

---
+++ frontend/sw.js
@@ -0,0 +1,16 @@
+// public/sw.js
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/logo.png',
+      data: { url: data.url }
+    })
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url || '/'));
+});
